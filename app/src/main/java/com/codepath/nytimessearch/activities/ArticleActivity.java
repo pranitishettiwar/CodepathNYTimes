bb8499f1281a81589b8cba867ed9daae1a02d732
pranitishettiwar <praniti@lumoslabs.com>
2017-09-20 23:54:44 -0700

    Fixed webview

--- app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
+++ app/src/main/java/com/codepath/nytimessearch/activities/ArticleActivity.java
@@ -1,9 +1,14 @@
 package com.codepath.nytimessearch.activities;
 
+import android.annotation.TargetApi;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.codepath.nytimessearch.R;
 import com.codepath.nytimessearch.model.Article;
@@ -11,6 +16,7 @@ import com.codepath.nytimessearch.model.Article;
 import org.parceler.Parcels;
 
 public class ArticleActivity extends AppCompatActivity {
+    private WebView webview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,20 +26,41 @@ public class ArticleActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         Article article = (Article) Parcels.unwrap(getIntent().getParcelableExtra("article"));
-        //String url = getIntent().getStringExtra("url");
 
         WebView webview = (WebView) findViewById(R.id.wvArticle);
 
-//        webview.setWebViewClient(new WebViewClient() {
-//                                     @Override
-//                                     public boolean shouldOverrideUrlLoading(WebView view, String url) {
-//                                         view.loadUrl(url);
-//                                         return true;
-//                                     }
-//                                 }
-//
-//        );
+        // Configure related browser settings
+        webview.getSettings().setLoadsImagesAutomatically(true);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        // Zoom out if the content width is greater than the width of the viewport
+        webview.getSettings().setLoadWithOverviewMode(true);
+
+        webview.getSettings().setSupportZoom(true);
+        webview.getSettings().setBuiltInZoomControls(true); // allow pinch to zooom
+
+        // Configure the client to use when opening URLs
+        webview.setWebViewClient(new MyBrowser());
+        // Load the initial URL
         webview.loadUrl(article.getWebUrl());
+    }
+
+    // Manages the behavior when URLs are loaded
+    private class MyBrowser extends WebViewClient {
+        @SuppressWarnings("deprecation")
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @TargetApi(Build.VERSION_CODES.N)
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+            view.loadUrl(request.getUrl().toString());
+            return true;
+        }
     }
 
 }
